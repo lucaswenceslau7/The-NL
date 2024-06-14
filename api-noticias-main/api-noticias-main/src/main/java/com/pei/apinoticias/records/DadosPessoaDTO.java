12934ea12312313e5685084e57f46212e731dc88
Lucas Wenceslau <79658874+lucaswenceslau7@users.noreply.github.com>
2024-06-13 21:33:07 -0300

    Delete api-noticias-main/api-noticias-main directory

--- api-noticias-main/api-noticias-main/src/main/java/com/pei/apinoticias/records/DadosPessoaDTO.java
+++
@@ -1,6 +0,0 @@
-package com.pei.apinoticias.records;
-
-import com.pei.apinoticias.enums.PreferenciaNoticia;
-
-public record DadosPessoaDTO (String nome, String CPF, String email, PreferenciaNoticia preferencia) {
-}
